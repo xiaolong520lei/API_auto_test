7396078d1eb596356e0fb0b7f570c3f1be7820f2
kevin <32223840>
2023-03-17 12:28:03 +0800

    判断项目路径是否已添加到sys.path中

--- run_main/main.py
+++ run_main/main.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import pytest
 from common.readConfig import read_config
 from common import allure_report
+
+object_path = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+if object_path not in sys.path:
+    sys.path.append(object_path)
 
 environment_info = read_config()['environment']
 case_path = os.path.join(os.path.dirname(os.getcwd()), 'test_case')
@@ -20,6 +25,6 @@ class AutoRunBaseInfo:
 if __name__ == '__main__':
     # print(case_path)
     # pytest.main(['-vv', '-s', case_path, '--alluredir', allure_report_data_path]) #本地运行方式
-    pytest.main(['-vv', '-s', case_path + '/test_login.py']) #临时测试jenkins
+    pytest.main(['-vv', '-s', case_path + '/test_login.py','--alluredir', allure_report_data_path]) #临时测试jenkins
     # allure_report.generate_report() #生成allure报告
 
